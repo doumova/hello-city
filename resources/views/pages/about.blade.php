b5ade676da14fa8715f23faa03d314c6888e58d9
doumova <doumogueye.mg@gmail.com>
2021-04-19 16:16:45 +0000

    using config

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('base')
 @section('content')
+@section('title','About Us | '.config('app.name '))
+
 <h2>About us</h2>
 <p>Bouild with &hearts; by GLobalt technologies</p>
 <p><a href="/">home page</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,6 @@
 @extends('base')
 @section('content')
+@section('title', config('app.name'))
 <h1>Hello from maroc</h1>
 <p>It's currently {{date('H:i A')}}</p>
 @endsection
