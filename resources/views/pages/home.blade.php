278c773b8526195933c44f23af829f8d0ccaa58f
doumova <doumogueye.mg@gmail.com>
2021-04-19 17:29:42 +0000

    add images with asset

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('base')
 @section('content')
-<img src="/images/flagmaroc.jpg" alt="flag maroc"/>
+<img src="{{asset('/images/flagmaroc.jpg')}}" alt="flag maroc"/>
 @section('title', config('app.name'))
 <h1>Hello from maroc</h1>
 <p>It's currently {{date('H:i A')}}</p>
