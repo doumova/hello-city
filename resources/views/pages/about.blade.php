e511e0d815b3e5817af125d004ac8ddf06d41b46
doumova <doumogueye.mg@gmail.com>
2021-04-19 15:44:29 +0000

    remove duplication by using yield

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
+@section('content')
+<h2>About us</h2>
+<p>Bouild with &hearts; by GLobalt technologies</p>
+<p><a href="/">home page</a></p>
 
-        <title>About Us | Hello City</title>
-    </head>
-    <body>
-        <p>Bouild with &hearts; by GLobalt technologies</p>
-        <p><a href="/">home page</a></p>
-
-        <footer>
-            <p>&copy; Copiright GlobalTech.SA {{date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Hello City</title>
-    </head>
-    <body>
-        <h1>Hello from maroc</h1>
-        <p>It's currently {{date('H:i A')}}</p>
-
-        <footer>
-            <p>
-                &copy; Copiright GlobalTech.SA {{date('Y')}} 
-                &middot; <a href="/about-us">ABOUT US</a>
-            </p>
-        </footer>
-    </body>
-</html>
+@extends('base')
+@section('content')
+<h1>Hello from maroc</h1>
+<p>It's currently {{date('H:i A')}}</p>
+@endsection
